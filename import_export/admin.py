7ec87a2ebe33671d3f1c51537cc399da7dece027
Olivier Cortès <contact@oliviercortes.com>
2015-06-09 16:09:26 +0200

    Same player shoots again.

--- import_export/admin.py
+++ import_export/admin.py
@@ -389,6 +389,9 @@ class ExportActionModelAdmin(ExportMixin, admin.ModelAdmin):
         """
         export_format = request.POST.get('file_format', base_formats.CSV)
 
+        if export_format == '':
+            export_format = base_formats.CSV
+
         if not export_format:
             messages.warning(request, _('You must select an export format.'))
         else:
